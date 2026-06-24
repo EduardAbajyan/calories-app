83e4553231619a73a5008aebad4104792f159811
Eduard Abajyan <edwardabajyan@gmail.com>
2026-06-25 00:11:45 +0400

    hydration issue about timezone

--- components/timezone-offset-input.tsx
+++ components/timezone-offset-input.tsx
@@ -9,11 +9,13 @@ type TimezoneOffsetInputProps = {
 export default function TimezoneOffsetInput({
   name = "tzOffsetMin",
 }: TimezoneOffsetInputProps) {
-  const [offset, setOffset] = useState("0");
+  const [offset, setOffset] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     setOffset(String(new Date().getTimezoneOffset()));
   }, []);
+
+  if (offset === undefined) return null;
 
   return <input type="hidden" name={name} value={offset} />;
 }
