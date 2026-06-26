9eb3646145815e67834bf30210ea4a077b302966
Eduard Abajyan <edwardabajyan@gmail.com>
2026-06-26 23:27:23 +0400

    deleted the donations link

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -288,34 +288,6 @@ export default function DashboardSidebar() {
 
             <li>
               <Link
-                href="/donations"
-                aria-disabled={pendingHref === "/donations"}
-                onClick={() => handleNavClick("/donations")}
-                className={linkClassName("/donations")}
-              >
-                <svg
-                  className="h-5 w-5 shrink-0 transition duration-75 group-hover:text-accent"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 21s-7-4.35-7-10a4 4 0 0 1 7-2.646A4 4 0 0 1 19 11c0 5.65-7 10-7 10Z"
-                  />
-                </svg>
-                <span className="ms-3">Donations</span>
-              </Link>
-            </li>
-
-            <li>
-              <Link
                 href="/dishes"
                 aria-disabled={pendingHref === "/dishes"}
                 onClick={() => handleNavClick("/dishes")}
